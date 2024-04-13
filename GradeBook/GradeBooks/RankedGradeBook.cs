715daf1f19dabc6a9ee68fe4e537d2ef944ad792
jkrokowska <czajkowska.joanna0@gmail.com>
2024-04-13 20:11:13 +0200

    Override `RankedGradeBook`'s `GetLetterGrade` method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -13,5 +13,26 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+            }
+
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);  // 20% of the total number of students
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (grades.IndexOf(averageGrade) < threshold)
+                return 'A';
+            else if (grades.IndexOf(averageGrade) < threshold * 2)
+                return 'B';
+            else if (grades.IndexOf(averageGrade) < threshold * 3)
+                return 'C';
+            else if (grades.IndexOf(averageGrade) < threshold * 4)
+                return 'D';
+            return 'F';
+        }
     }
 }
